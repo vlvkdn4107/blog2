0bf7c2c86be5f18301dc0cf727b1a198a12239fa
vlvkdn4107 <102271558+vlvkdn4107@users.noreply.github.com>
2022-06-30 14:10:32 +0900

    first commit

---
+++ src/main/java/com/blog_test/Tencoblog2Application.java
@@ -0,0 +1,13 @@
+package com.blog_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tencoblog2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tencoblog2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/blog_test/Tencoblog2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tencoblog2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
